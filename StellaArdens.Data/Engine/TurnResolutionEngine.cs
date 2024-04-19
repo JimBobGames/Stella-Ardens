b1f9389d562c7ed2cd3d7436a669b824112006b1
Jamie <gamesforjamie@gmail.com>
2024-04-19 15:11:32 +0100

    new version

---
+++ StellaArdens.Data/Engine/TurnResolutionEngine.cs
@@ -0,0 +1,24 @@
+﻿using StellaArdens.Data.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellaArdens.Data.Engine
+{
+    public class TurnResolutionEngine
+    {
+        public void RunTurn()
+        {
+
+        }
+
+        public RaceReport GenerateReportForRace(int raceId)
+        {
+            RaceReport r = new RaceReport();
+
+            return r;
+        }
+    }
+}
---
+++ StellaArdens.Data/Game/NetworkedStellaArdensGame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StellaArdens.Data.Game
+{
+    public class NetworkedStellaArdensGame : AbstractStellaArdensGame
+    {
+    }
+}
