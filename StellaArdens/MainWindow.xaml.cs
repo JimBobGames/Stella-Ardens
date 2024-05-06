5a9bbda945bde3c49a483705e822119aadd289e6
Jamie <gamesforjamie@gmail.com>
2024-05-06 11:40:48 +0100

    dd

--- StellaArdens/MainWindow.xaml.cs
+++ StellaArdens/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using StellaArdens.Data.Engine;
 using StellaArdens.Data.Game;
+using StellaArdens.Data.Objects;
 using StellaArdens.Data.Reports;
 using System.Text;
 using System.Windows;
@@ -30,7 +31,32 @@ namespace StellaArdens
             controller.Game = TestGameCreator.CreateTestGame();
             controller.RaceReport = controller.TurnResolutionEngine.GenerateReportForRace(controller.Game.Player.RaceId, controller.Game);
 
+            // generate debug map
+            MapTab.IsSelected = true;
+            GenerateDebugMap(controller.Game);
+
             UpdateAfterTurn();
+        }
+
+        private void GenerateDebugMap(IStellaArdensGame game)
+        {
+            MapCanvas.Children.Clear();
+           
+            Ellipse e1;
+
+            foreach (SolarSystem ss in game.SolarSystems)
+            {
+                e1 = new Ellipse();
+                e1.Height = 50;
+                e1.Width = 50;
+                e1.Stroke = Brushes.Red;
+                e1.StrokeThickness = 5;
+                Canvas.SetLeft(e1, ss.Location.X * 100);
+                Canvas.SetTop(e1, ss.Location.Y * 100);
+                MapCanvas.Children.Add(e1);
+            }
+
+            
         }
 
         public void UpdateAfterTurn()
@@ -51,6 +77,16 @@ namespace StellaArdens
                 controller.RunTurn();
                 UpdateAfterTurn();
             }
+        }
+
+        private void MainTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // when the tab is changed
+        }
+
+        private void MapTab_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+           // MapTab
         }
     }
 }
